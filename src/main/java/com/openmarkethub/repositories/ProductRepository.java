60eb0fab12af81a525ecbbf46fff2b81a8ad14e1
dhunganabibek <Dhunganabibek2016@gmail.com>
2023-02-28 22:26:37 -0600

    features added

--- src/main/java/com/openmarkethub/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.openmarkethub.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.openmarkethub.entities.Product;
-
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
--- src/main/java/com/openmarkethub/security/CustomUserDetailService.java
+++
@@ -1,5 +0,0 @@
-package com.openmarkethub.security;
-
-public class CustomUserDetailService {
-
-}
